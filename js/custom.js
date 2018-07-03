9e715fe7f4afe81850101f0da9a63323b1f9528a
Your Name <pavankunchalapk@gmail.com>
2018-07-03 11:52:47 +0530

    completed team

--- js/custom.js
+++ js/custom.js
@@ -25,14 +25,15 @@ $(function () {
                     TEAM
 ================================================*/
 
-$(function (){
-    
+$(function () {
+
     $("#team-members").owlCarousel({
-    items: 3,
-        autoplay: true, 
+        items: 3,
+        autoplay: true,
         smartSpeed: 700,
         loop: true,
-        
+        autoplayHoverPause: true,
+
     });
-    
+
 });
