3f19655fb70e96addcd7de76d68aea0c00708e8e
Your Name <pavankunchalapk@gmail.com>
2018-07-09 22:10:11 +0530

    perfecting

--- js/custom.js
+++ js/custom.js
@@ -103,3 +103,19 @@ $(function () {
     });
 
 });
+
+
+//close mobile menu on click
+
+
+$(function () {
+
+    $(".navbar-collapse ul li a").on("click touch", function(){
+        
+        $(".navbar-toggle").click();
+    });
+
+});
+
+
+
